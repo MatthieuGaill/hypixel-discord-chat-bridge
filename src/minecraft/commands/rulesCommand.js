83facea9e482bbcf8433372da070950d1176ee68
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2023-11-23 22:32:13 +0100

    Create rulesCommand.js

---
+++ src/minecraft/commands/rulesCommand.js
@@ -0,0 +1,21 @@
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
+
+class RulesCommand extends minecraftCommand {
+  constructor(minecraft) {
+    super(minecraft);
+
+    this.name = "rules";
+    this.aliases = ["r"];
+    this.description = "Give the rules";
+    ];
+  }
+
+  async onCommand(username, message) {
+      this.send(`/gc Respect the others. No phishing/sexual/illegal content. Avoid swearing. No racial/xenophobic/homophobic comment. No toxicity/flood/spam (including whole sentences in caps). No begging (even in pm).`);
+    } catch (error) {
+      this.send(`/gc [ERROR] ${error}`);
+    }
+  }
+}
+
+module.exports = RulesCommand;

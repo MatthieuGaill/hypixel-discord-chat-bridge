13532a2b8b859b257fc0c43c268b1c515e16d0d7
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2023-11-23 22:22:02 +0100

    Update kickCommand.js

--- src/discord/commands/kickCommand.js
+++ src/discord/commands/kickCommand.js
@@ -37,8 +37,8 @@ module.exports = {
       .setAuthor({ name: "Kick" })
       .setDescription(`Successfully executed \`/g kick ${name} ${reason}\``)
       .setFooter({
-        text: `by @duckysolucky | /help [command] for more information`,
-        iconURL: "https://imgur.com/tgwQJTX.png",
+        text: `/help [command] for more information`,
+        iconURL: "https://i.imgur.com/Fc2R9Z9.png",
       });
 
     await interaction.followUp({

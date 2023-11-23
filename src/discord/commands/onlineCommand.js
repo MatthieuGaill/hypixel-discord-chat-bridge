56df8fb2267055d6f5b3f64b2460d02d9640691e
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2023-11-23 22:22:28 +0100

    Update onlineCommand.js

--- src/discord/commands/onlineCommand.js
+++ src/discord/commands/onlineCommand.js
@@ -57,8 +57,8 @@ module.exports = {
       .setTitle("Online Members")
       .setDescription(description)
       .setFooter({
-        text: "by @duckysolucky | /help [command] for more information",
-        iconURL: "https://imgur.com/tgwQJTX.png",
+        text: "/help [command] for more information",
+        iconURL: "https://i.imgur.com/Fc2R9Z9.png",
       });
 
     return await interaction.followUp({ embeds: [embed] });

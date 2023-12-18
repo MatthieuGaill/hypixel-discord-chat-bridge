474f950c63f2d5c1a874fba46699b60666d480b4
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2023-12-19 00:49:13 +0100

    Update demoteCommand.js

--- src/discord/commands/demoteCommand.js
+++ src/discord/commands/demoteCommand.js
@@ -31,8 +31,8 @@ module.exports = {
       .setAuthor({ name: "Demote" })
       .setDescription(`Successfully executed \`/g demote ${name}\``)
       .setFooter({
-        text: `by @duckysolucky | /help [command] for more information`,
-        iconURL: "https://imgur.com/tgwQJTX.png",
+        text: `/help [command] for more information`,
+        iconURL: "https://i.imgur.com/Fc2R9Z9.png",
       });
 
     await interaction.followUp({

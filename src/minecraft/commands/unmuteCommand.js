f0c94cbb23cdd721b485b4524b2a81a78830965e
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2023-12-18 23:50:25 +0100

    Update unmuteCommand.js

--- src/minecraft/commands/unmuteCommand.js
+++ src/minecraft/commands/unmuteCommand.js
@@ -1,4 +1,3 @@
-
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const { getUUID } = require("../../contracts/API/PlayerDBAPI.js");
@@ -9,9 +8,9 @@ class UnmuteCommand extends minecraftCommand {
   constructor(minecraft) {
     super(minecraft);
 
-    this.name = "Unmmute";
+    this.name = "unmute";
     this.aliases = ["um"];
-    this.description = "Unmute player (staff only)";
+    this.description = "Unmute player for a given duration (staff only)";
     this.options = [
       {
         name: "username",
@@ -23,6 +22,7 @@ class UnmuteCommand extends minecraftCommand {
 
   async onCommand(username, message) {
     try {
+      let isRemove = false;
       const uuid = await getUUID(username);
       const mod_list = config.minecraft.commands.mod_list;
       if (!mod_list.includes(uuid)){
@@ -32,10 +32,24 @@ class UnmuteCommand extends minecraftCommand {
       if (!arg[0]) {
         this.send("/gc Wrong Usage: !unmute [name]");
       }
-      username = arg[0];
-      this.send(`/g unmute ${username}`);
-      await delay(1000);
-
+      const muted_username = arg[0];
+      const muteListener = async (message) => {
+           message = message.toString();
+           if (message.includes("has muted")){
+             this.send(`/gc ${muted_username} has been unmuted`);
+             await delay(2000);
+           }else if (message.includes("cannot") || message.includes("find") || message.includes("Invalid")){
+              this.send(`/gc [ERROR] ${message}`);
+              bot.removeListener("message", muteListener);
+              isRemove = true;
+           }
+      };
+      bot.on("message", muteListener);
+      this.send(`/g unmute ${muted_username}`);
+      await delay(4000);
+      if (isRemove === false){
+        bot.removeListener("message", muteListener);    
+      }
     } catch (error) {
       this.send(`/gc [ERROR] ${error}`);
     }

582a14848be216b800c95206dccc459f7e7b94bd
MatthieuGaill <125156256+MatthieuGaill@users.noreply.github.com>
2024-02-20 22:37:04 +0100

    Update translateCommand.js

--- src/minecraft/commands/translateCommand.js
+++ src/minecraft/commands/translateCommand.js
@@ -1,25 +1,7 @@
-onst minecraftCommand = require("../../contracts/minecraftCommand.js");
+const minecraftCommand = require("../../contracts/minecraftCommand.js");
 const {Translate} = require('@google-cloud/translate').v2;
+const fs = require("fs");
 require('dotenv').config();
-
-// Commands
-const commands = [
-  "!8ball", "!8b", "!accesories","!acc", "!talismans","!talisman",
-  "!auction", "!ah", "!auctions", "!bedwars", "!bw", "!bws",
-  "!bestiary", "!be","!calculate", "!calc", "!math",
-  "!catacombs", "!cata", "!dungeons", "!duels", "!duel", "!fairysouls",
-  "!fs", "!fetchur", "!guild", "!g","!guildexp","!gexp",
-  "!help", "!info", "!hypixel", "!h", "!kick", "!k", "!kitty",
-  "!cat", "!cutecat", "!megawalls", "!mw", "!mute", "!m",
-  "!networth", "!nw", "!quickmaths","!qm", "!armor",
-  "!equipment", "!render", "!inv", "!i",
-  "!inventory", "!pet", "!pets", "!rules", "!r", "!skills",
-  "!skill", "!sa", "!skyblock", "!stats", "!sb", "!level",
-  "!lvl", "!skywars", "!sw", "!slayer", "!slayers",
-  "!translate", "!ts", "!UHC", "!uhc", "!unmute", "!um",
-  "!unscramble", "!us", "!unscrambleme", "!warpout", "!warp",
-  "!weight", "!w", "!woolwars", "!ww"
-]
 
 // Your credentials
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS)
@@ -45,8 +27,16 @@ class TranslateCommand extends minecraftCommand {
       },
     ];
   }
+    
   async onCommand(username, message) {
     const arg = this.getArgs(message);
+    const commands = fs.readdirSync("./src/minecraft/commands")
+          .filter(file => file.endsWith(".js") && file != "translateCommand.js")
+          .map(file => {
+            let instance = new (require(`./${file}`))()
+            return [instance.name, ...instance.aliases].map(elem => "!" + elem)
+          }).flat();
+    
     try {
       if (arg.length === 0) {
         // eslint-disable-next-line no-throw-literal
@@ -55,6 +45,7 @@ class TranslateCommand extends minecraftCommand {
       const text = arg.join(' ');
       commands.forEach(command => {
         if (text.includes(command)) {
+          console.log(`command : ${command}`);
           throw "Please refrain from using a command in translation";
         }
       })
